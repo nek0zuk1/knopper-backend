498f608064e45ae22b5d6a4da8b60cbd96c761a8
aj <aljosephpaz17@gmail.com>
2026-03-06 21:56:51 +0800

    try ko si lumanang code

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['MYSQL_HOST'] = os.getenv('DB_HOST')
 app.config['MYSQL_USER'] = os.getenv('DB_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('DB_PASSWORD') 
 app.config['MYSQL_DB'] = os.getenv('DB_NAME')
-app.config['MYSQL_PORT'] = int(os.getenv('DB_PORT', 30250))
+app.config['MYSQL_PORT'] = int(os.getenv('DB_PORT'))
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET') 
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=12)
 
